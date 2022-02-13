c132d76eb841dbaae5660853ff88cd3a0b0d43d0
Jannis <mattlage.jannis@gmail.com>
2022-02-13 23:10:51 +0100

    comments todo

--- ExamManager/Program.cs
+++ ExamManager/Program.cs
@@ -20,34 +20,21 @@ namespace ExamManager
             // student/teacher object
             // tabindex
             // daten als liste exportieren
-            // idea: Count exams per date in listviewexams
-            // teacher subject dropdown
-            // (lehrer doppelnamen)
-            // editexam btn left top (1klick-> show exam(-> block boxes))
+            // export data to txt file
             // entertaste in textbox
             // doppelnamen filter -> fehlersuche
-            // check lehrer und schüler nur in einem raum zur zeit
-            // Lehrzeichen Plathalter
             // allstudents filter/order + better layout
-            // delete old exams funktion
-            // prep room can be null
+            // delete old exams funktion + Form
             // color theme
-            // export data to txt file
             // if get student > 1 -> error! + Message
             // connection status?
-            // suche tooltipp -> more info
-            // check if vorbereitungsraum is empty
-            // remove addexam if id=null
+            // remove addexam if id=null ?
             // unterschiedliche prüfungsarten (1 schueler, 2/3 schueler)
-            // exam doppelclick -> open edit and mark exam in timeline
-            // teacher email
+            // teacher email ?
             // exam gleicher lehrer
-            // fächer lehrer filtern -> WARNUNG -> +ausnahmen
             // edit students form faster load
-            // readteacherfile -> add subject
             // select teacher -> delete multiple
             // filter grade order abc
-            // show filter
             // filter teacher
             // hide timelines without exams in filter
             // rerender timeline
